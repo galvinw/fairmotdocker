b509b3b9c90d0c7c35e299270640a25fab78191e
endysusanto13 <endy.susanto@gmail.com>
2022-01-10 19:20:55 +0800

    Remove unnecessary files: old FairMOT, monoloco and monofair

--- monofair/README.md
+++
@@ -1,2 +0,0 @@
-Transfer model to monofair/modelsx folder
-Transfer mp4 to monofair/ folder
--- monofair/monoloco/network/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .pifpaf import PifPaf, ImageList
-from .net import MonoLoco
-from .process import unnormalize_bi, extract_outputs_mono, extract_labels
