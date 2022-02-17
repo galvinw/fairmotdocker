9fef65a408493e8b9465962f45e6736c86673f29
endysusanto13 <endy.susanto@gmail.com>
2022-02-17 23:06:43 +0800

    Update db schema

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 # app/db.py
 
+from email.policy import default
 import databases
 import ormar
 import sqlalchemy
@@ -9,43 +10,31 @@ from datetime import datetime
 
 from pydantic import BaseSettings, Field
 
-
 class Settings(BaseSettings):
     db_url: str = Field(..., env='DATABASE_URL')
 
 settings = Settings()
 
-
 database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
-
 
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
     database = database
 
-
 class User(ormar.Model):
     class Meta(BaseMeta):
-        tablename = "users"
+        tablename = "user"
     id: int = ormar.Integer(primary_key=True)
-    email: str = ormar.String(max_length=128, unique=True, nullable=False)
-    active: bool = ormar.Boolean(default=True, nullable=False)
-    create_at: datetime = ormar.DateTime(default=datetime.now) 
+    username: str = ormar.String(max_length=128, unique=True, nullable=False)
+    is_active: bool = ormar.Boolean(default=True, nullable=False)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
 
-class Zones(ormar.Model):
+class Camera(ormar.Model):
     class Meta(BaseMeta):
-        tablename = "zones"
-    id: int = ormar.Integer(primary_key=True)
-    name: str = ormar.String(max_length=128,nullable=False, default="Zone")
-    camera_id: int = ormar.Integer(nullable=False, default=1)
-    x: int = ormar.Integer(nullable=False, default=800)
-    y: int = ormar.Integer(nullable=False, default=600)
-    create_at: datetime = ormar.DateTime(default=datetime.now) 
-
-class Cameras(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "cameras"
+        tablename = "camera"
     id: int = ormar.Integer(primary_key=True)
     name: str = ormar.String(max_length=128,nullable=False, default="Camera")
     connectionstring: str = ormar.String(max_length=256,nullable=False)
@@ -53,34 +42,69 @@ class Cameras(ormar.Model):
     lat:float = ormar.Float(nullable=True)
     long:float = ormar.Float(nullable=True)
     camera_shift_time: int = ormar.Integer( default=0)
-    lastchange: datetime = ormar.DateTime(default=datetime.now) 
-    create_at: datetime = ormar.DateTime(default=datetime.now) 
-
-class PersonInstance(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "person_instance"
-    id: int = ormar.Integer(primary_key=True)
-    name: str = ormar.String(max_length=128,nullable=False, default="PersonInstance")
-    frame_id: int = ormar.Integer(nullable=True)
-    x:float = ormar.Float(nullable=True)
-    z:float = ormar.Float(nullable=True)
-    create_at: datetime = ormar.DateTime(default=datetime.now)
+    focal_length:float = ormar.Float(nullable=True)
+    # lastchange: datetime = ormar.DateTime(default=datetime.now) 
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
 
 class Person(ormar.Model):
     class Meta(BaseMeta):
         tablename = "person"
     id: int = ormar.Integer(primary_key=True)
     name: str = ormar.String(max_length=128,nullable=False, default="Person")
-    create_at: datetime = ormar.DateTime(default=datetime.now) 
+    is_active: bool = ormar.Boolean(default=True, nullable=False)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
 
-class Zone_Status(ormar.Model):
+class PersonInstance(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "person_instance"
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=128,nullable=False)
+    camera_id: int = ormar.Integer(nullable=True)       # Not for production
+    frame_id: int = ormar.Integer(nullable=True)        # Not for production
+    conf_level:float = ormar.Float(nullable=True)
+    status: str = ormar.String(max_length=128,nullable=True)
+    position_x:float = ormar.Float(nullable=True)
+    position_z:float = ormar.Float(nullable=True)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
+
+class Zone(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "zone"
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=128,nullable=False, default="Zone")
+    camera_id: int = ormar.Integer(nullable=False, default=1)
+    zone_x1: int = ormar.Integer(nullable=False, default=0)
+    zone_y1: int = ormar.Integer(nullable=False, default=0)
+    zone_x2: int = ormar.Integer(nullable=False, default=0)
+    zone_y2: int = ormar.Integer(nullable=False, default=0)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
+
+class ZoneStatus(ormar.Model):
     class Meta(BaseMeta):
         tablename = "zone_status"
     id: int = ormar.Integer(primary_key=True)
-    zone_id: str = ormar.Integer(nullable=False, default=1)
-    number: str = ormar.Integer(nullable=False, default=1)
-    create_at: datetime = ormar.DateTime(default=datetime.now) 
+    zone_id: str = ormar.Integer(nullable=False, default=0)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
 
+class PersonZoneStatus(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "person_zone_status"
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=128,nullable=False)
+    zone_id: str = ormar.Integer(nullable=False)
+    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    created_at: datetime = ormar.DateTime(default=datetime.now)
+    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
 
 
 engine = sqlalchemy.create_engine(settings.db_url)
