1c0cbb393bad4f8f05afbb14f83f1f179f8b43c3
endysusanto13 <endy.susanto@gmail.com>
2022-02-24 20:42:27 +0800

    Add Request models

--- app/db.py
+++ app/db.py
@@ -1,16 +1,15 @@
 # app/db.py
 
+import pydantic
 import databases
 import ormar
 import sqlalchemy
-
-from datetime import datetime
-
 from config import settings
+from pydantic import Json
+from datetime import datetime
 
 database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
-
 
 class BaseModel(ormar.Model):
     class Meta:
@@ -18,75 +17,129 @@ class BaseModel(ormar.Model):
         metadata = metadata
         database = database
     
-    id: int = ormar.Integer(primary_key=True)
-    created_at: datetime = ormar.DateTime(default=datetime.now)
-    updated_at: datetime = ormar.DateTime(default=None, nullable=True)
-    is_deleted: bool = ormar.Boolean(default=False, nullable=False)
+    id         : int        = ormar.Integer  (primary_key=True)
+    created_at : datetime   = ormar.DateTime (default=datetime.now)
+    updated_at : datetime   = ormar.DateTime (default=None, nullable=True)
+    is_deleted : bool       = ormar.Boolean  (default=False, nullable=False)
 
 class User(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "user"
 
-    username: str = ormar.String(max_length=128, unique=True, nullable=False)
-    is_active: bool = ormar.Boolean(default=True, nullable=False)
+    username   : str    = ormar.String  (max_length=128, unique=True, nullable=False)
+    is_active  : bool   = ormar.Boolean (default=True, nullable=False)
 
 class Camera(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "camera"
 
-    name: str = ormar.String(max_length=128,nullable=False, default="Camera")
-    connectionstring: str = ormar.String(max_length=256,nullable=False)
-    threshold: int = ormar.Integer(nullable=True)
-    lat:float = ormar.Float(nullable=True)
-    long:float = ormar.Float(nullable=True)
-    camera_shift_time: int = ormar.Integer( default=0)
-    focal_length:float = ormar.Float(nullable=True)
-    # lastchange: datetime = ormar.DateTime(default=datetime.now) 
+    name                : str   = ormar.String  (max_length=128,nullable=False, default="Camera")
+    connectionstring    : str   = ormar.String  (max_length=256,nullable=False)
+    threshold           : int   = ormar.Integer (nullable=True)
+    lat                 : float = ormar.Float   (nullable=True)
+    long                : float = ormar.Float   (nullable=True)
+    camera_shift_time   : int   = ormar.Integer (default=0)
+    focal_length        : float = ormar.Float   (nullable=True)
+    # lastchange          : datetime = ormar.DateTime(default=datetime.now) 
 
 class Person(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "person"
 
-    name: str = ormar.String(max_length=128,nullable=False, default="Person")
-    is_active: bool = ormar.Boolean(default=True, nullable=False)
+    strack_id   : int   = ormar.Integer (index=True, nullable=False)
+    name        : str   = ormar.String  (max_length=128,nullable=False, default="Person")
+    is_active   : bool  = ormar.Boolean (default=True, nullable=False)
 
 class PersonInstance(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "person_instance"
 
-    person_id: int = ormar.Integer(nullable=True)
-    name: str = ormar.String(max_length=128,nullable=False)
-    camera_id: int = ormar.Integer(nullable=True)       # Not for production
-    frame_id: int = ormar.Integer(nullable=True)        # Not for production
-    conf_level:float = ormar.Float(nullable=True)
-    status: str = ormar.String(max_length=128,nullable=True)
-    position_x:float = ormar.Float(nullable=True)
-    position_z:float = ormar.Float(nullable=True)
+    strack_id   : int   = ormar.Integer (index=True, nullable=False)
+    name        : str   = ormar.String  (max_length=128,nullable=False)
+    camera_id   : int   = ormar.Integer (nullable=True)                  # Not for production
+    frame_id    : int   = ormar.Integer (nullable=True)                  # Not for production
+    conf_level  : float = ormar.Float   (nullable=True)
+    status      : str   = ormar.String  (max_length=128,nullable=True)
+    position_x  : float = ormar.Float   (nullable=True)
+    position_z  : float = ormar.Float   (nullable=True)
 
 class Zone(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "zone"
 
-    name: str = ormar.String(max_length=128,nullable=False, default="Zone")
-    camera_id: int = ormar.Integer(nullable=False, default=1)
-    zone_x1: int = ormar.Integer(nullable=False, default=0)
-    zone_y1: int = ormar.Integer(nullable=False, default=0)
-    zone_x2: int = ormar.Integer(nullable=False, default=0)
-    zone_y2: int = ormar.Integer(nullable=False, default=0)
+    name          : str  = ormar.String  (max_length=128,nullable=False, default="Zone")
+    camera_id     : int  = ormar.Integer (nullable=False, default=1)
+    coordinates   : Json = ormar.JSON    (nullable=False)
 
 # class ZoneStatus(BaseModel):
 #     class Meta(ormar.ModelMeta):
 #         tablename = "zone_status"
 
-#     zone_id: str = ormar.Integer(nullable=False, default=0)
+#     zone_id               : str = ormar.Integer(nullable=False, default=0)
 
 class PersonZoneStatus(BaseModel):
     class Meta(ormar.ModelMeta):
         tablename = "person_zone_status"
 
-    name: str = ormar.String(max_length=128,nullable=False)
-    zone_id: str = ormar.Integer(nullable=False)
+    strack_id   : int = ormar.Integer   (nullable=False)
+    person_name : str = ormar.String    (max_length=128,nullable=False)
+    zone_id     : int = ormar.Integer   (index=True, nullable=False)
 
 
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
+
+
+class RequestUser(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    username    : str
+
+class RequestCamera(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    name                : str
+    connectionstring    : str
+    threshold           : int
+    lat                 : float
+    long                : float
+    camera_shift_time   : int
+    focal_length        : float
+
+class RequestPerson(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    strack_id   : int
+    name        : str
+
+class RequestPersonInstance(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    strack_id   : int  
+    name        : str  
+    camera_id   : int  
+    frame_id    : int  
+    conf_level  : float
+    status      : str  
+    position_x  : float
+    position_z  : float
+
+class RequestPersonZoneStatus(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    strack_id   : int
+    person_name : str
+    zone_id     : int
+
+class RequestZone(pydantic.BaseModel):
+    class Config:
+        orm_mode = True
+
+    name          : str  
+    camera_id     : int  
+    coordinates   : Json 
